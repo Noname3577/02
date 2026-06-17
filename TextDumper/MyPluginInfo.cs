0d854d4981a258a489a0248c85a390dffb9877d1
LV.1 <zone3577zone@gmail.com>
2026-06-18 00:46:11 +0700

    Delete TextDumper directory

--- TextDumper/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TextDumper
-{
-    internal static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "com.textdumper.yakuzarogue";
-        public const string PLUGIN_NAME = "TextDumper";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
