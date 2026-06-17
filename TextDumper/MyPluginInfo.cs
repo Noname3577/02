4ef3a26b3aa1d9ee96f9d9d5fdf2e240c44cca4e
LV.1 <zone3577zone@gmail.com>
2026-06-17 14:24:50 +0700

    Add files via upload

---
+++ TextDumper/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TextDumper
+{
+    internal static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "com.textdumper.yakuzarogue";
+        public const string PLUGIN_NAME = "TextDumper";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
